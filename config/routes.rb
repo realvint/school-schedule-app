07027cd1b3ea86457c98e5f943c33c37da6bfaee
Victor Malukhin <victor.malukhin@gmail.com>
2023-05-22 21:51:27 +0300

    Gem devise basic installation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_up: 'registration' }
+
   root 'static_pages#landing_page'
 
   get 'privacy_policy', to: 'static_pages#privacy_policy'
