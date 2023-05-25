2da80fcdfbdc6e27af4dcc68c762880fdb12124a
Victor Malukhin <victor.malukhin@gmail.com>
2023-05-25 16:05:36 +0300

    Add social log in with Github (Omniauth)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable
   devise :database_authenticatable, :registerable, :trackable,
-         :rememberable, :validatable
+         :rememberable, :validatable, :omniauthable, omniauth_providers: [:github]
+
+  def self.from_omniauth(access_token)
+    user = User.where(email: access_token.info.email).first
+
+    user ||= User.create(
+      email: access_token.info.email,
+      password: Devise.friendly_token[0, 20]
+    )
+    user
+  end
 end
